46bef04464f52ce82f956bce432cc0da2f78788a
Shinnosuke-Hara <dkm1873@gmail.com>
2023-03-24 05:30:11 +0900

    create database

--- src/constants/genre-list.ts
+++ src/constants/genre-list.ts
@@ -2,17 +2,121 @@ export const genreList: Genre[] = [
   {
     id: 'japanese',
     label: '和食',
-    image: '/genre-images/japanese.jpeg',
+    image: '/genre-images/1.jpeg',
   } as const,
   {
     id: 'chinese',
     label: '中華',
-    image: '/genre-images/chinese.jpeg',
+    image: '/genre-images/2.jpeg',
   } as const,
-  { id: 'sushi', label: '寿司', image: '/genre-images/sushi.jpeg' } as const,
+  {
+    id: 'sushi',
+    label: '寿司',
+    image: '/genre-images/3.jpeg',
+  } as const,
   {
     id: 'yakiniku',
     label: '焼肉',
-    image: '/genre-images/yakiniku.jpeg',
+    image: '/genre-images/4.jpeg',
+  } as const,
+  {
+    id: 'byuhhye',
+    label: 'ビュッフェ',
+    image: '/genre-images/5.jpeg',
+  } as const,
+  {
+    id: 'italian',
+    label: 'イタリアン',
+    image: '/genre-images/6.jpeg',
+  } as const,
+  {
+    id: 'french',
+    label: 'フレンチ',
+    image: '/genre-images/7.jpeg',
+  } as const,
+  {
+    id: 'korian',
+    label: '韓国料理',
+    image: '/genre-images/8.jpeg',
+  } as const,
+  {
+    id: 'yumcha',
+    label: 'ユンチャ',
+    image: '/genre-images/9.jpeg',
+  } as const,
+  {
+    id: 'yakitori',
+    label: '焼き鳥',
+    image: '/genre-images/10.jpeg',
+  } as const,
+  {
+    id: 'sukiyaki',
+    label: 'すき焼き',
+    image: '/genre-images/11.jpeg',
+  } as const,
+  {
+    id: 'teppanyaki',
+    label: '鉄板焼き',
+    image: '/genre-images/12.jpeg',
+  } as const,
+  {
+    id: 'steak',
+    label: 'ステーキ',
+    image: '/genre-images/13.jpeg',
+  } as const,
+  {
+    id: 'thai',
+    label: 'タイ料理',
+    image: '/genre-images/14.jpeg',
+  } as const,
+  {
+    id: 'unagi',
+    label: 'うなぎ',
+    image: '/genre-images/15.jpeg',
+  } as const,
+  {
+    id: 'udon',
+    label: 'うどん',
+    image: '/genre-images/16.jpeg',
+  } as const,
+  {
+    id: 'niku',
+    label: '肉',
+    image: '/genre-images/17.jpeg',
+  } as const,
+  {
+    id: 'cafe',
+    label: 'カフェ',
+    image: '/genre-images/18.jpeg',
+  } as const,
+  {
+    id: 'shurasuko',
+    label: 'シュラスコ',
+    image: '/genre-images/19.jpeg',
+  } as const,
+  {
+    id: 'jingisukan',
+    label: 'ジンギスカン',
+    image: '/genre-images/20.jpeg',
+  } as const,
+  {
+    id: 'gyokai',
+    label: '魚介',
+    image: '/genre-images/21.jpeg',
+  } as const,
+  {
+    id: 'chese',
+    label: 'チーズ',
+    image: '/genre-images/22.jpeg',
+  } as const,
+  {
+    id: 'bear',
+    label: 'ビアバー',
+    image: '/genre-images/23.jpeg',
+  } as const,
+  {
+    id: 'bbq',
+    label: 'バーベキュー',
+    image: '/genre-images/24.jpeg',
   } as const,
 ]
