0759abfb2f31235e6ee2aaba0e146a6565c8e78f
Shinnosuke-Hara <dkm1873@gmail.com>
2023-03-24 03:30:29 +0900

    feat database

---
+++ src/constants/Restauants.ts
@@ -0,0 +1,13 @@
+import Resuauant01 from '@/assets/01.png'
+import { Restauant } from '../../@types/Restaurant'
+
+export const RESTAUANTS: Restauant[] = [
+  {
+    id: '1',
+    name: 'ヴィーネリア ルーナルーチェ',
+    location: 1,
+    src: Resuauant01,
+    description:
+      'ヴィーネリア ルーナルーチェは、ヴィーネリアの中心部に位置する、ヴィーネリアの名物の一つである、ルーナルーチェを提供するレストランです。',
+  },
+]
